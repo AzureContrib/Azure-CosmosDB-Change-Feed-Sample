b0abaf891c78903542abe7f5f15972feb316e37f
Nithin Mohan T K <nithin@thingx.cloud>
2018-10-04 18:03:11 +0100

    Added a .NET core and FX version.

--- Contoso.ProcessorConsole/Constants.cs
+++ Contoso.ProcessorConsole/Constants.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+ using System.Configuration;
 
 namespace Contoso.ProcessorConsole
 {
     public class Constants
     {
-          
+        public const string CosmosDb_DatabaseName = "Products";
+        public const string CosmosDb_CollectionName = "Groceries";
+        public const string CosmosDb_Uri = "https://thingx-retail-store-db.documents.azure.com:443/";
+
+        public const string CosmosDb_Key =
+            "3dR2jyxEIVMVgYl1hT5CToFHGSGnQbHEW9bVz3yvx2YGFeVGvD8yn0utLo23XIH8WmzmLWW8KuZk1DhVt9lGqA==";
     }
 }
---
+++ Contoso.ProcessorConsoleNETFx/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contoso.ProcessorConsole.NETFx
+{
+    public class Constants
+    {
+          public const string CosmosDb_DatabaseName = "Products";
+          public const string CosmosDb_CollectionName = "Groceries";
+          public const string CosmosDb_Uri = "https://thingx-retail-store-db.documents.azure.com:443/";
+          public const string CosmosDb_Key = "3dR2jyxEIVMVgYl1hT5CToFHGSGnQbHEW9bVz3yvx2YGFeVGvD8yn0utLo23XIH8WmzmLWW8KuZk1DhVt9lGqA==";
+
+    }
+}
